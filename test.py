3397378386fa097c8b6175c9e477bca9771651ca
atognoo_b <133734424+atognoob@users.noreply.github.com>
2025-01-08 17:52:04 +0700

    Update test.py

--- test.py
+++ test.py
@@ -25,6 +25,6 @@ print(response.json())
 print("\nTesting POST /api/v1/datediff")
 response = requests.post('http://127.0.0.1:8000/api/v1/datediff', json={
     "start": {"date": "1.1.2023 10:30:00", "tz": "Asia/Tokyo"},
-    "end": {"date": "1.26.2023 10:30:00", "tz": "Europe/Paris"}
+    "end": {"date": "1.26.2023 9:34:00", "tz": "Europe/Paris"}
 })
 print(response.json())
